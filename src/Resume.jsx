3dde4f969756935d5d39185b768ec31494f8d8ea
Anthony Zardis <anthonyzardis@gmail.com>
2019-08-27 13:51:53 -0700

    Resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,9 +2,84 @@ import React from 'react'
 
 export default function Resume() {
   return (
-    <div>
-      <h1>THIS WILL BE WHERE THE RESUME SHOWS UP!</h1>
-      <h1>THIS WILL BE WHERE THE RESUME SHOWS UP!</h1>
+    <>
+    <div className="resume-container">
+      <div className="resume-header">
+        <h1>ANTHONY ZARDIS</h1>
+        <h3>ANTHONYZARDIS@GMAIL.COM | 206.313.8631| SEATTLE, WA</h3>
+      </div>
+    <div className='resume-body'>
+      <div>
+        <h3>Summary</h3><hr/>
+        <p>
+          Resourceful entry level Software Engineer, enthusiastic about developing practical and time saving software solutions. 
+          Adept at identifying issues with existing software and creating innovate new software programs. A creative and talented leader; 
+          committed to working cross-functionally with all stakeholders to deliver meticulous execution of the final project.
+        </p>
+      </div>
+      <div>
+        <h3>Technical Skills</h3><hr/>
+        <p>JavaScript, Node, Git, HTML, CSS, SQL, Postgres, Mongo, Mongoose, Ruby, React. Microsoft Suite, G-Suite</p>
+      </div>
+      <div>
+        <h3>Skills</h3><hr/>
+        <p>Project Management • Customer Service • Budget Management • Training & Development • Hydraulic System Design • Payroll • Vendor Management • Customer Experience • Operations Coordination • Team Management • Journeyman Machinist • Accounts Receivable/Payable • Custom Fabrication • Financial Reporting</p>
+      </div>
+      <div>
+        <h3>Education</h3><hr/>
+        <p>Central Washington University, 2016 Bachelors, Supply Chain Management </p>
+      </div>
+      <div>
+        <h3>Certifications</h3><hr/>
+        <p>Software Development Immersive | Bloc, 2017</p>
+        <p>Software Engineering Immersive | General Assembly, 2019</p>
+      </div>
+      <div>
+        <h3>Experience</h3><hr/>
+        <h5>Puget Sound Hydraulics</h5>
+        <h5>Project Manager, 2008-2016</h5>
+        <p>Expanded my knowledge base to achieve master level as both a Machinist and Mechanic. Advanced to Project Management and led multiple complex job sites on strict production deadlines while maintaining quality standards. Specialized in efficient project turnaround to maximize productivity in addition to profits.</p>
+        <ul>
+          <li>Cultivate and maintain customer relationships by providing superior customer service and high-quality craftsmanship.</li>
+          <li>Manage vendor relationships for efficient procurement of materials on multiple simultaneous projects.</li>
+          <li>Direct cross-functional teams of internal as well as external staff at multiple sites.</li>
+          <li>Knowledge and ability to work with a variety of materials including but not limited to stainless steel, brass, steel, aluminum, and manganese bronze.</li>
+          <li>Accurately forecast needs of multiple job sites to keep fast turnaround timeline. </li>
+          <li>Design new products as well as re-design and improvements to existing.</li>
+          <li>Delegate key tasks while working with a variety of personality types.</li>
+          <li>Research industry trends for improvement of best practices and completive analysis.</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Western Peterbilt</h3><hr/>
+        <h5>Accounting Associate, 2005-2008</h5>
+        <p>Held responsibility for accurate preparation, analysis, and reporting of accounts receivables, payables, payroll, warranty work, and customer accounts. Worked as a team player and was instrumental in streamlining office functions to enhance productivity and maximize financial control.</p>
+        <ul>
+          <li>Managed $400,000+ in accounts daily for a multitude of customers and companies.</li>
+          <li>Continually improved the customer experience by delivering top-tier customer service.</li>
+          <li>Efficiently managed customer accounts and mitigated outstanding debts.</li>
+          <li>Calculated, tracked, and input all relative financial information for 100+ employees.</li>
+          <li>Drafted invoices, reports, and vendor payments in a timely manner.</li>
+          <li>Managed financial statements and organized data for executives.</li>
+          <li>Resolved transactional and invoice issues in collaboration with my colleagues.</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Carter Volkswagen</h3><hr/>
+        <h5>Accounting Clerk, 2001-2005</h5>
+        <p>Utilized my strong mathematical skills and ability to manage complex systems and large amounts of data. Fostered and maintained long-term relationships with customers and vendors by providing exceptional customer service. Simultaneously managed current and outstanding receivables and warranties, while keeping current on all balance and month-end reporting.</p>
+        <ul>
+          <li>Implemented structured accounting techniques.</li>
+          <li>Maintained a calendar of events for effective long-term planning and management.</li>
+          <li>Continuously improved the overall experience while providing exceptional customer service. </li>
+          <li>Assisted accountants with quarterly and year-end closing.</li>
+          <li>Provided strong clerical support to senior staff.</li>
+          <li>Organized hundreds of invoices and purchase orders for daily reconciliation.</li>
+          <li>Settled warranty accounts and analyzed accounts receivable reports.</li>
+        </ul>
+      </div>
     </div>
+    </div>
+    </>
   )
 }
