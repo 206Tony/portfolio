8d1bd8a950f8038dc5037d2f5ff03033136315f1
Anthony Zardis <anthonyzardis@gmail.com>
2019-09-11 13:54:05 -0700

    Add Photo

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import IMG_0264 from './IMG_0264.JPG';
 
 export default function About() {
   return (
-    <div>
+    <div className="about-me">
       <h1>About Me</h1>
+      <div className="my-pic">
+        <img src={IMG_0264}  alt="AZardis"/>
+      </div><br/>
       <p>My name is Anthony Zardis I am a junior web developer!</p>
       <p>Me PacMan</p>
     </div>
