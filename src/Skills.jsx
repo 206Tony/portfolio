bd0fbd25c16f505ffbb7ce7651fd824013e62ac0
Anthony Zardis <anthonyzardis@gmail.com>
2019-09-25 15:46:20 -0700

    Skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -3,15 +3,20 @@ import React from 'react'
 export default function Skills() {
   return (
     <div className="skills-container">
-      <p><img src="https://img.icons8.com/color/2x/ruby-programming-language.png" alt="Ruby"/><br/> Ruby</p>
-      <p><img src="https://img.icons8.com/color/2x/python.png" alt="Python"/><br/>Python</p>
-      <p><img src="https://img.icons8.com/color/2x/sql.png" alt="SQL"/><br/>SQL</p>
+      <p><img src="https://img.icons8.com/color/2x/react-native.png" alt="React"/><br/>React</p>
+      <p><img src="https://img.icons8.com/color/2x/html-5.png" alt="HTML5"/><br/> HTML5</p>
+      <p><img src="https://img.icons8.com/color/2x/css3.png" alt="CSS3"/><br/>CSS3</p>
       <p><img src="https://img.icons8.com/color/2x/javascript.png" alt="JavaScript"/><br/>JavaScript</p>
-      <p><img src="https://img.icons8.com/material-outlined/2x/json.png" alt="JSON"/><br/>JSON</p>
-      <p><img src="https://img.icons8.com/metro/2x/html.png" alt="HTML"/><br/> HTML</p>
+      <p><img src="https://img.icons8.com/color/2x/ruby-programming-language.png" alt="Ruby"/><br/>Ruby</p>
+      <p><img src="https://img.icons8.com/color/2x/python.png" alt="Python"/><br/>Python</p>
+      <p><img src="https://cdn.iconscout.com/icon/free/png-256/typescript-1174965.png" style={{width: 95.99, height: 95.99, margin: 8}} alt="Typescript"/><br/>Typescript</p>
       <p><img src="https://img.icons8.com/color/2x/nodejs.png" alt="node"/><br/> NODE</p>
-      {/* <img src="" alt=""/> */}
-      
+      <p><img src="https://img.icons8.com/color/2x/json.png" alt="JSON"/><br/>JSON</p>
+      <p><img src="https://img.icons8.com/color/2x/sql.png" alt="SQL"/><br/>SQL</p>
+      <p><img src="https://img.icons8.com/color/2x/postgreesql.png" alt="PostgreSQL"/><br/>PostgreSQL</p>
+      <p><img src="https://img.icons8.com/color/2x/mongodb.png" alt="MongoDB"/><br/>MongoDB</p>
+      <p><img src="https://cdn.iconscout.com/icon/free/png-256/express-2-282577.png" style={{width: 95.99, height: 95.99, margin: 8}} alt="Express"/><br/>Express</p>
+      <p><img src="https://cdn.iconscout.com/icon/free/png-256/django-13-1175187.png" style={{width: 95.99, height: 95.99, margin: 8}} alt="Django"/><br/>Django</p>
     </div>
   )
 }
