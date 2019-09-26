eebf57b2e599d6bf4c023155b6e7749ae0bd6ad5
Anthony Zardis <anthonyzardis@gmail.com>
2019-09-25 20:06:57 -0700

    Contact Links

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,9 +5,9 @@ import React from 'react'
     <div style={{color: "black"}}>
       <h1>Anthony Zardis</h1>
       <h3 style={{color: "black"}}> <a href="email:anthonyzardis@gmail.com" target="_blank" rel="noopener noreferrer">anthonyzardis@gmail.com</a></h3>
-      <h3><a href="https://www.linkedin.com/in/anthony-zardis" target="_blank" rel="noopener noreferrer" style={{color: "black"}}> LinkedIn </a></h3>
-      <h3><a href="https://www.github.com/206tony" target="_blank" rel="noopener noreferrer" style={{color: "black"}}> GitHub </a></h3>
-      <h3><a href="tel:206-313-8631" target="_blank" rel="noopener noreferrer">206.313.8631</a></h3>
+      <h3 style={{color: "black"}}><a href="https://www.linkedin.com/in/anthony-zardis" target="_blank" rel="noopener noreferrer" style={{color: "black"}}> LinkedIn </a></h3>
+      <h3 style={{color: "black"}}><a href="https://www.github.com/206tony" target="_blank" rel="noopener noreferrer" style={{color: "black"}}> GitHub </a></h3>
+      <h3 style={{color: "black"}}><a href="tel:206-313-8631" target="_blank" rel="noopener noreferrer">206.313.8631</a></h3>
 
     </div>
   )
